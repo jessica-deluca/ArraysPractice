2ee238d5e342d5a3a08fc1d765b9fa0a9bca24af
Your Name <you@example.com>
2017-07-05 11:09:39 -0400

    added class practice problems

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -8,8 +8,55 @@ namespace ArraysPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // this line means main method with a string array of arguments
         {
+
+            //int[] myFirstArray; // declaring array without initializing
+            //string[] myStringArray = { "first", "second", "third", "fourth" }; // declaring & initializing array
+            //char[] myLastName = { 'd', 'e', 'l', 'u', 'c', 'a' }; // declaring & initializing a char array with the letters of my last name // remember char is single quotes
+
+            //// print 3 elements of my array
+            //Console.WriteLine(myLastName[0]); // tells console which index to print; console prints the element of the 0 index
+            //Console.WriteLine(myLastName[3]);
+            //Console.WriteLine(myLastName[1]);
+
+            // create the following arrays and print using the correct index numbers;
+
+            // array of top ten vacation spots; print the first and last vacation spots
+
+            //string[] vacationSpots = { "Australia", "Dubai", "Hawaii", "Iceland", "Cleveland", "Los Angeles", "San Diego", "Aruba", "Jamaica", "Cancun" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+
+            // array of the ages of 3 children you know; print each child's age
+
+            // int[] childAge = { 5, 10, 2 };
+            //Console.WriteLine(childAge[0]);
+            //Console.WriteLine(childAge[1]);
+            //Console.WriteLine(childAge[2]);
+
+            // Console.WriteLine(childAge[0] + childAge[1] + childAge[2]); // this will add the numbers together
+            // Console.WriteLine(childAge[0] + " " + childAge[1] + " " + childAge[2]); // this will print them on the same line with spaces between the two
+
+            //// array of GPA's on a 4.0 scale of 8 students (you don't have to print)
+
+            //double[] gpa = { 2.5, 3.0, 4.0, 2.7, 3.5, 3.2, 3.9, 3.2 };
+
+            // array of the first letter of 4 of your classmates' names who are sitting near you; print all four on the same line
+
+            // char[] classmateNames = { 'j', 'f', 'j', 'm' };
+
+            // Console.WriteLine(classmateNames); // this only works with characters & there will not be spaces between the characters
+
+            // Console.Write(classmateNames[0]); // remove "Line" in "WriteLine" to print characters on the same line; line break is after the "Write" or "WriteLine"
+            // Console.Write(classmateNames[1]);
+            // Console.Write(classmateNames[2]);
+            // Console.Write(classmateNames[3]);
+
+            // Console.WriteLine("{0}, {1}, {2}, {3}", classmateNames[0], classmateNames[1], classmateNames[2], classmateNames[3]); // prints them on the same line; commas inbetween {} print commas between the characters
+
+
+
         }
     }
 }
